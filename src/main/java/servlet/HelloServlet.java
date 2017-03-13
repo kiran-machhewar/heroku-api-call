9abce3e1394d184378f6959fc97b232bb6edf920
Kiran Machhewar <kiran_machhewar@persistent.com>
2017-03-12 21:35:55 -0600

    make it better

---
+++ src/main/java/servlet/HelloServlet.java
@@ -0,0 +1,30 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import myjarpackage.MyJarService;
+
+@WebServlet(
+        name = "MyServlet", 
+        urlPatterns = {"/hello"}
+    )
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        ServletOutputStream out = resp.getOutputStream();
+        String outputFromJar = MyJarService.getOutputFromJar();
+        out.write(("JavaWebAppTemplateHeroku\n "+outputFromJar).getBytes());
+        out.flush();
+        out.close();
+    }
+    
+}
